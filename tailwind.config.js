c92538e3eaa0a27d0289ed693cb7cd4ec1370930
Monika Bysiak <monika.serwin88@gmail.com>
2023-09-14 14:52:49 +0200

    wrong navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'page': '#020916',
+        'pink': '#FF61EF',
+      },
+    },
   },
   plugins: [],
 }
