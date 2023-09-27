d342d0039e93e377b4769f7f31de2b9ce50c327e
Monika Bysiak <monika.serwin88@gmail.com>
2023-09-27 12:42:51 +0200

    completing project--js

--- src/projects.js
+++ src/projects.js
@@ -5,7 +5,38 @@ fetch(` https://api.github.com/users/${username}/repos`)
 .then(response => response.json())
 .then(response => {
     for (let repository of response) {
+        console.log(repository)
         const {name, stargazers_count, html_url, description, topics } = repository;
+        const element = `<article
+                class="rounded-lg md:rounded-xl overflow-clip bg-gradient-to-br from-white/10 to-white/5 flex flex-col h-full">
+                <div
+                    class="h-11 bg-gradient-to-br from-white/30 to-white/20 shadow-inner-light ring-1 ring-slate-800 rounded-t-lg">
+                    <span class="p-4 flex flex-row justify-start gap-1.5 items-center"> <img
+                            src="./span.project__circle.png"><img src="./span.project__circle.png"><img
+                            src="./span.project__circle.png"></span>
+                </div>
+                <div
+                    class="h-auto bg-gradient-to-br p-5 md:p-6 lg:p-10 from-white/10 to-white/10 shadow-inner-light ring-1 ring-slate-800 rounded-b-xl flex flex-col grow justify-between">
+                    <header class="flex gap-4 items-center mb-4">
+                        <h3 class="text-white font-bold text-2xl"> ${name} </h3>
+                        <p class="px-2 py-1 gap-0.5 flex items-center bg-gray/10 text-gray rounded"> <img
+                                class="w-4 h-4" src="./star-01 1.png"> ${stargazers_count} </p>
+                    </header>
+                    <p class="text-gray mb-4 text-xl"> ${description} </p>
+                    <ul class="flex gap-2 flex-wrap mb-10">
+                        <li class="text-white font-bold text-sm px-2 py-1 bg-gray/10 rounded"> ${topics} </li>
+                    </ul>
+                    <div class="flex flex-row flex-wrap gap-4 justify-start">
+                        <a
+                            class="text-pink bg-page py-4 px-5 font-bold text-base md:text-xl rounded-2xl flex gap-3 items-center border-gray-800 border-2 transition-color duration-500 hover:border-pink border-zinc-500">
+                            <img src="./Vector.png"> View demo </a>
+                        <a href= "${html_url}" target="_blank" target="noreferrer nofollow"
+                            class="text-pink bg-page py-4 px-5 font-bold text-base md:text-xl rounded-2xl flex gap-3 items-center border-gray-800 border-2 transition-color duration-500 hover:border-pink border-zinc-500">
+                            <img src="./github.png"> Source code </a>
+                    </div>
+                </div>
+            </article>`
+            repositoriesContainer.insertAdjacentHTML('beforeend', element)
     }
 })
 .catch((e) => {console.log(e)})
